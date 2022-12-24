a06c2f1c134f33e133f001beb17181e53672c5ab
YOON <yoon@YOONui-MacBookPro.local>
2022-12-24 18:59:43 +0900

    [김고은] 불필요한 주석 제거 및 내용 수정

--- Frontend/git-rebase-test/src/pages/F1.js
+++ Frontend/git-rebase-test/src/pages/F1.js
@@ -3,11 +3,10 @@ import React from "react";
 const F1 = () => {
   return (
     <div>
-      <h1>F1 페이지</h1>
-      <div>hello world </div>
+      <h1>F1 페이지 입니다~</h1>
+      <div>hello world and everyone! </div>
       <button>추가</button>
       <button>추가2</button>
-      {/* 주석주석 */}
     </div>
   );
 };

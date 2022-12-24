52f900e81b6c0eebc3b6377f1490f5e493ae284a
YOON <yoon@YOONui-MacBookPro.local>
2022-12-24 17:24:48 +0900

    [김개발] 리스트페이지 완료 및 commit history 정리 완료

--- Frontend/git-rebase-test/src/Router.js
+++ Frontend/git-rebase-test/src/Router.js
@@ -2,18 +2,28 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Main from "./pages/Main/Main";
+<<<<<<< HEAD
 import Detail from "./pages/Detail/Detail";
+=======
+import List from "./pages/List/List";
+>>>>>>> 6c03647 ([김개발] 리스트페이지 레이아웃 완료)
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+<<<<<<< HEAD
         <Route path="/" element={<Main />} />
         <Route path="/signin" element={<Login />} />
 <<<<<<< HEAD
 =======
         <Route path="/detail" element={<Detail />} />
 >>>>>>> a1da926 ([윤선영] ADD : detail 페이지에 버튼 추가)
+=======
+        <Route path="/main" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/list" element={<List />} />
+>>>>>>> 6c03647 ([김개발] 리스트페이지 레이아웃 완료)
       </Routes>
     </BrowserRouter>
   );
---
+++ Frontend/git-rebase-test/src/pages/List/List.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <div>
+      나는 리스트페이지 나는 리스트페이지
+      <input placeholder="hi" />
+      <input placeholder="hi" />
+      <h1>지금 rebase 연습중</h1>
+      <input placeholder="hi" />
+    </div>
+  );
+};
+
+export default List;

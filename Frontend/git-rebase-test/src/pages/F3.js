b09bf39a437ae6a2b57f4b05a5b35eb10d196d83
YOON <yoon@YOONui-MacBookPro.local>
2022-12-24 19:29:11 +0900

    [김철수] ADD : f3 페이지 완료

---
+++ Frontend/git-rebase-test/src/pages/F3.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const F3 = () => {
+  return (
+    <div>
+      <button> 2 </button>
+      <button> 3 </button>
+      <button> 4 </button>
+      <h1>F2 페이지</h1>
+      <div>리베이스 이제 좀 알것 같아요</div>
+      <button> 1 </button>
+    </div>
+  );
+};
+
+export default F3;

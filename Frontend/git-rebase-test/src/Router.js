54a9b4bac139231facc9023a99e0890ad9054b10
YOON <yoon@YOONui-MacBookPro.local>
2022-12-24 17:15:10 +0900

    [윤선영] 디테일 페이지 완료 및 git commit history 정리

--- Frontend/git-rebase-test/src/Router.js
+++ Frontend/git-rebase-test/src/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Main from "./pages/Main/Main";
+import Detail from "./pages/Detail/Detail";
 
 export default function Router() {
   return (
@@ -9,6 +10,10 @@ export default function Router() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/signin" element={<Login />} />
+<<<<<<< HEAD
+=======
+        <Route path="/detail" element={<Detail />} />
+>>>>>>> a1da926 ([윤선영] ADD : detail 페이지에 버튼 추가)
       </Routes>
     </BrowserRouter>
   );

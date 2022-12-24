062961f63f20ba5f512254b57fdfc84521985f08
YOON <yoon@YOONui-MacBookPro.local>
2022-12-25 00:27:21 +0900

    [윤선영] Add : Nav 수정

--- Frontend/git-rebase-test/src/pages/Nav.js
+++ Frontend/git-rebase-test/src/pages/Nav.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div>
+      <h1>여기는 네브바입니다</h1>
+      <div>주저리주저리</div>
+    </div>
+  );
 };
 
 export default Nav;

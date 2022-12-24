7f8bc39d7ac91f872ac73924099534fae280668e
YOON <yoon@YOONui-MacBookPro.local>
2022-12-25 00:26:06 +0900

    [윤선영] Add : Nav 구현완료

--- Frontend/git-rebase-test/src/Router.js
+++ Frontend/git-rebase-test/src/Router.js
@@ -8,31 +8,25 @@ import F1 from "./pages/F1";
 import F2 from "./pages/F2";
 import F3 from "./pages/F3";
 import F5 from "./pages/F5";
-<<<<<<< HEAD
 import F7 from "./pages/F7";
-=======
 import F6 from "./pages/F6";
->>>>>>> b630cad ([윤선영] Add: F6 페이지 레이아웃 구현)
+import Nav from "./pages/Nav";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/main" element={<Main />} />
+        <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/detail" element={<Detail />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/list" element={<List />} />
         <Route path="/f1" element={<F1 />} />
         <Route path="/f2" element={<F2 />} />
         <Route path="/f3" element={<F3 />} />
         <Route path="/f5" element={<F5 />} />
-<<<<<<< HEAD
         <Route path="/f7" element={<F7 />} />
-=======
         <Route path="/f6" element={<F6 />} />
->>>>>>> b630cad ([윤선영] Add: F6 페이지 레이아웃 구현)
+        <Route path="/nav" element={<Nav />} />
       </Routes>
     </BrowserRouter>
   );

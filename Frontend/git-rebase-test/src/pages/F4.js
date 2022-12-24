66d5855337f3956096e05206851f829c58584621
YOON <yoon@YOONui-MacBookPro.local>
2022-12-24 19:59:09 +0900

    [yoon] Add: f4페이지 기능 구현 및 파일 경로 수정

---
+++ Frontend/git-rebase-test/src/pages/F4.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const f4 = () => {
+  return (
+    <div>
+      <input />
+      <input />
+      <input />
+      <input />
+    </div>
+  );
+};
+
+export default f4;

ea4bd594a3444e18b76933af62b30d25cf324e78
YOON <yoon@YOONui-MacBookPro.local>
2022-12-24 19:07:21 +0900

    [이나라] Add : f2 페이지 완료 및 commit history 정리

--- Frontend/git-rebase-test/src/Router.js
+++ Frontend/git-rebase-test/src/Router.js
@@ -4,15 +4,22 @@ import Login from "./pages/Login/Login";
 import Main from "./pages/Main/Main";
 import Detail from "./pages/Detail/Detail";
 import List from "./pages/List/List";
+import F2 from "./pages/F2";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+<<<<<<< HEAD
+=======
+        <Route path="/main" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+>>>>>>> 896cd43 ([이나라] Add : 찐막)
         <Route path="/detail" element={<Detail />} />
         <Route path="/main" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/list" element={<List />} />
+        <Route path="/f2" element={<F2 />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ Frontend/git-rebase-test/src/pages/F2.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const F2 = () => {
+  return (
+    <div>
+      <button> hi everyone!</button>
+      <button> hi</button>
+      <h2>hello world</h2>
+    </div>
+  );
+};
+
+export default F2;

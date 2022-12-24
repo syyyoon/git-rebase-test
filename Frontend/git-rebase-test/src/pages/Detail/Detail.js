19c93f96f719bb8ab1886e4990676dd615e24183
YOON <yoon@YOONui-MacBookPro.local>
2022-12-24 17:15:10 +0900

    [윤선영] 디테일 페이지 완료 및 git commit history 정리

---
+++ Frontend/git-rebase-test/src/pages/Detail/Detail.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Detail = () => {
+  return (
+    <div>
+      나는 디테일페이지!!!
+      <button>나는 버튼 1</button>
+      <button>나는 버튼 2</button>
+      <button>나는 버튼 3</button>
+    </div>
+  );
+};
+
+export default Detail;

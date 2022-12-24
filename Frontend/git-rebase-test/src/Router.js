40d50c5d2924cc962bfc53956adb6977bbe5a0b0
YOON <yoon@YOONui-MacBookPro.local>
2022-12-24 16:47:19 +0900

    [윤선영] ADD : 로그인 페이지 완료 및 commit history 정리 + router에서 발생한 conflict 해결

--- Frontend/git-rebase-test/src/Router.js
+++ Frontend/git-rebase-test/src/Router.js
@@ -7,8 +7,13 @@ export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+<<<<<<< HEAD
         <Route path="/main" element={<Main />} />
         <Route path="/login" element={<Login />} />
+=======
+        <Route path="/" element={<Main />} />
+        <Route path="/signin" element={<Login />} />
+>>>>>>> dceaf50 ([윤선영] ADD : 로그인 페이지 완료 및 commit history 정리)
       </Routes>
     </BrowserRouter>
   );

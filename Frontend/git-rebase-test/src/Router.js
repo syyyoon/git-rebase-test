49acb1529e66291f1945fa48fdbcc1eef2328ba6
YOON <yoon@YOONui-MacBookPro.local>
2022-12-24 19:55:22 +0900

    [yoon] Add: f4페이지 레이아웃 완료

--- Frontend/git-rebase-test/src/Router.js
+++ Frontend/git-rebase-test/src/Router.js
@@ -7,6 +7,7 @@ import List from "./pages/List/List";
 import F1 from "./pages/F1";
 import F2 from "./pages/F2";
 import F3 from "./pages/F3";
+import f4 from "./pages/Main/f4";
 
 export default function Router() {
   return (
@@ -21,6 +22,7 @@ export default function Router() {
         <Route path="/f1" element={<F1 />} />
         <Route path="/f2" element={<F2 />} />
         <Route path="/f3" element={<F3 />} />
+        <Route path="/f4" element={<F4 />} />
       </Routes>
     </BrowserRouter>
   );
